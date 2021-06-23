41ca4535323e32125dadb54a4f8c9c3185ff6e76
Desenvolve Fácil <suporte@desenvolvefacil.com.br>
2021-06-23 12:09:23 -0300

    Correção no código de verificação pix

--- dfpix.php
+++ dfpix.php
@@ -126,6 +126,28 @@ function dfpix_link($params) {
     $pix .= "6304"; //Adiciona o campo do CRC no fim da linha do pix.
     $pix .= crcChecksum($pix); //Calcula o checksum CRC16 e acrescenta ao final.
     
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
     //gera o qrcode do PIX
     ob_start();
     QRCode::png($pix, null, 'M', 5);
